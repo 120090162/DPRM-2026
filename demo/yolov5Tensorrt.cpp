7ef1c46b2b9813dc4e2458d45c716adf499f4fa2
Chr0mium24 <3294212389@qq.com>
2025-10-13 15:38:26 +0800

    test

--- demo/yolov5Tensorrt.cpp
+++ demo/yolov5Tensorrt.cpp
@@ -131,6 +131,7 @@ int main(int argc, char* argv[]) {
     std::filesystem::path onnx_filepath(params.onnx_path);
     std::cout << "ONNX filepath: " << std::endl;
     std::string engine_filepath = onnx_filepath.replace_extension(".engine").string();
+    std::cout << "engine_filepath" << std::endl;
     std::cout << "TensorRT engine filepath: " << engine_filepath << std::endl;
     // 如果 .engine 文件不存在，则从 ONNX 文件构建它
     if (!file_exists(engine_filepath)) {
